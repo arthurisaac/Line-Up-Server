0b2a31808c4931ff7da070d587d4cb5ae655981b
Arthur <r.thur.light@gmail.com>
2020-09-25 17:25:07 +0000

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const database = mysql.createConnection({
-    host: "fasobizness.com",
-    user: "c1292278c_lu",
-    password: "Sourir@rt24",
-    database: "c1292278c_lu"
+    host: "	sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ah057kfjbkolz4do",
+    password: "jaf89al1zf8viseg",
+    database: "qooalzvdkvh3byxb"
     /*host: "localhost",
     user: "root",
     password: "root",
